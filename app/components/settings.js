6d9ee37c66439b421081472af4fb728ca29bffb0
Stephen von Takach <steve@cotag.me>
2014-11-20 17:50:23 +1100

    lighting page complete

--- app/components/settings.js
+++ app/components/settings.js
@@ -21,287 +21,159 @@
     window.systemData = window.systemData || {};
     window.systemData['sys-B0'] = {
         System: [{
-            "$powerup": function () {
-                this.state = "online";
+            $powerup: function () {
+                this.state = "basic";
+                this.tab = 'lights';
             },
-            "$shutdown": function () {
+            $shutdown: function () {
                 this.state = "shutdown";
             },
             $tab: function (tab) {
                 this.tab = tab;
             },
-            $present: function (source, display) {
-                var src = this.sources[source];
+            $present: function (source) {
+                var display = 'Display_1';
 
-                // Return source and source title
-                this[display] = {
-                    source: source,
-                    title: src.title,
-                    type: src.type
-                };
+                if (source === 'none') {
+                    this[display] = {
+                        source: 'none'
+                    };
 
-                // Clear mute
-                if (!this.outputs[display].no_mod) {
-                    var parts = getModuleParts(display);
-                    this.$_self[parts.module][parts.index - 1].mute = false;
+                    // Mute the display as well
+                    if (!this.outputs[display].no_mod) {
+                        var parts = getModuleParts(display);
+                        this.$_self[parts.module][parts.index - 1].mute = true;
+                    }
+                } else {
+                    var src = this.sources[source];
+
+                    // Return source and source title
+                    this[display] = {
+                        source: source,
+                        title: src.title,
+                        type: src.type
+                    };
+
+                    // Clear mute
+                    if (!this.outputs[display].no_mod) {
+                        var parts = getModuleParts(display);
+                        this.$_self[parts.module][parts.index - 1].mute = false;
+                    }
                 }
             },
-            $video_mute: function (display) {
-                this[display] = {
-                    source: 'none'
-                };
-
-                if (!this.outputs[display].no_mod) {
-                    var parts = getModuleParts(display);
-                    this.$_self[parts.module][parts.index - 1].mute = true;
+            $newState: function (pass) {
+                if (pass == '1234') {
+                    this.state = 'client';
+                    this.tab = 'vision';
+                } else if (pass == 'admin') {
+                    this.state = 'advanced';
+                } else {
+                    // inform user of issue
+                    // TODO:: notification system
                 }
             },
             "name": "Demo Room",
             "help_msg": "For help please call <strong>0408419954</strong>",
-            "state": "shutdown",
-            "tab": "Camera",
-            "inputs": [
-                "PC",
-                "Laptop",
-                "Camera",
-                "VC"
-            ],
-            "PC": [
-                "g1_pc1",
-                "g1_pc2",
-                "g2_pc1",
-                "g2_pc2"
-            ],
-            "Laptop": [
-                "laptop_g1",
-                "laptop_g2"
-            ],
-            "Camera": [
-                "cam_r_g1",
-                "cam_f_g1",
-                "cam_r_g2",
-                "cam_f_g2"
-            ],
-            "VC": [
-                "vc1",
-                "vc2"
-            ],
+            "state": "shutdown", // basic, booked, advanced
+            "tab": "Lights",
+            "tabs": {
+                "basic": [
+                    "lights",
+                    "help",
+                    "start"
+                ],
+                "client": [
+                    "lights",
+                    "vision", // DualVision for dual displays
+                    "audio",
+                    "help",
+                    "end"
+                ],
+                "advanced": [
+                    "lights",
+                    "vision", // DualVision for dual displays
+                    "audio",
+                    "end"
+                ]
+            },
+            "pages": {
+                "lights": {
+                    "title": "Lighting"
+                },
+                "vision": {
+                    "title": "Vision"
+                },
+                "audio": {
+                    "title": "Audio"
+                },
+                "start": {
+                    "title": "Start Meeting"
+                },
+                "end": {
+                    "title": "End Meeting"
+                },
+                "help": {
+                    "title": "Help"
+                }
+            },
             "sources": {
-                "g1_pc1": {
-                    "title": "1G1 PC-1",
-                    "input": 14,
-                    "source": "hdmi",
-                    "type": "residentpc"
-                },
-                "g1_pc2": {
-                    "title": "1G1 PC-2",
-                    "input": 15,
-                    "source": "hdmi",
-                    "type": "residentpc"
-                },
-                "g2_pc1": {
-                    "title": "1G2 PC-1",
+                "respc": {
+                    "title": "Resident PC",
                     "input": 1,
                     "source": "hdmi",
                     "type": "residentpc"
                 },
-                "g2_pc2": {
-                    "title": "1G2 PC-2",
-                    "input": 5,
-                    "source": "hdmi",
-                    "type": "residentpc"
-                },
-                "laptop_g1": {
-                    "title": "1G1 Laptop",
-                    "input": 11,
-                    "source": "hdmi",
-                    "type": "hdmi"
-                },
-                "laptop_g2": {
-                    "title": "1G2 Laptop",
+                "laptop": {
+                    "title": "Client Laptop",
                     "input": 2,
                     "source": "hdmi",
-                    "type": "hdmi"
+                    "type": "laptop"
                 },
-                "cam_r_g1": {
-                    "title": "1G1 Rear",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 1,
-                    "input": 12,
-                    "source": "hdmi"
-                },
-                "cam_f_g1": {
-                    "title": "1G1 Front",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 2,
-                    "input": 13,
-                    "source": "hdmi"
-                },
-                "cam_r_g2": {
-                    "title": "1G2 Rear",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 3,
-                    "input": 10,
-                    "source": "hdmi"
-                },
-                "cam_f_g2": {
-                    "title": "1G2 Front",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 4,
-                    "input": 9,
-                    "source": "hdmi"
-                },
-                "vc1": {
-                    "title": "1G1 VC",
-                    "type": "vc-active",
-                    "input": 6,
-                    "content": 8
-                },
-                "vc2": {
-                    "title": "1G2 VC",
-                    "type": "vc-active",
-                    "input": 3,
-                    "content": 6
+                "vga": {
+                    "title": "VGA Cable",
+                    "source": "vga",
+                    "type": "vga"
                 }
             },
             "outputs": {
                 "Display_1": {
+                    "type": "projector",
                     "screen": "Screen_1",
                     "output": [
-                        3,
-                        23
+                        1
                     ],
-                    "audio_out": 21,
-                    "mixer_id": 105,
-                    "type": "projector",
-                    "pri": 1,
-                    "title": "G1 Front"
-                },
-                "Display_2": {
-                    "screen": "Screen_2",
-                    "output": [
-                        4,
-                        24
-                    ],
-                    "audio_out": 22,
-                    "mixer_id": 32,
-                    "type": "projector",
-                    "pri": 2,
-                    "title": "G1 Rear"
-                },
-                "VidConf_1": {
-                    "output": [
-                        7,
-                        9
-                    ],
-                    "no_audio": true,
-                    "no_mod": true,
-                    "type": "conference",
-                    "pri": 3,
-                    "title": "G1 VC"
-                },
-                "Display_3": {
-                    "screen": "Screen_3",
-                    "output": [
-                        1,
-                        21
-                    ],
-                    "audio_out": 23,
-                    "mixer_id": 107,
-                    "type": "projector",
-                    "pri": 4,
-                    "title": "G2 Front",
-                    "remote": true
-                },
-                "Display_4": {
-                    "screen": "Screen_4",
-                    "output": [
-                        2,
-                        22
-                    ],
-                    "audio_out": 24,
-                    "mixer_id": 106,
-                    "type": "projector",
-                    "pri": 5,
-                    "title": "G2 Rear",
-                    "remote": true
-                },
-                "VidConf_2": {
-                    "output": [
-                        5
-                    ],
-                    "no_audio": true,
-                    "no_mod": true,
-                    "type": "conference",
-                    "pri": 6,
-                    "title": "G2 VC",
-                    "remote": true
+                    "title": "Demo Room Projector"
                 }
             },
             "light_level": "Off",
-            "lights": {
-                "levels": [
-                    {
-                        "name": "Full",
-                        "trigger": 3
-                    },
-                    {
-                        "name": "Presentation",
-                        "trigger": 2
-                    },
-                    {
-                        "name": "Off",
-                        "trigger": 1
-                    }
-                ],
-                "default": 3,
-                "shutdown": 0
-            },
-            "vol_max": 3,
-            "vol_min": -50
-        }],
-        Mixer: [{
-            fader_106: 1,
-            fader_107: 2,
-            fader_105: 3,
-            fader_32: -30,
-            fader_107_mute: true, 
-            $fader: function (fader, volume) {
-                this['fader_' + fader] = volume;
-            },
-            $mute: function (fader, mute) {
-                this['fader_' + fader + '_mute'] = mute;
-            } 
+            "light_levels": [
+                "Off",
+                "Presentation",
+                "Discussion",
+                "Full"
+            ],
+            "light_presets": {
+                "Off": {
+                    "trigger": 1,
+                    "message": "Room lights are off.<br />Please select a preset"
+                },
+                "Presentation": {
+                    "trigger": 2,
+                    "message": "Lights are on Presentation mode"
+                },
+                "Discussion": {
+                    "trigger": 3,
+                    "message": "Lights are on Discussion mode"
+                },
+                "Full": {
+                    "trigger": 4,
+                    "message": "Lights are at full brightness"
+                }
+            }
         }],
         Switcher: [{}],
         Lights: [{}],
-        Computer: [{}, {}, {}, {}],
-        Camera: [{
-            joy_right: 50,
-            joy_left: 1,
-            joy_center: 25,
-            zoom: 25
-        }, {
-            joy_right: 50,
-            joy_left: 1,
-            joy_center: 25,
-            zoom: 1
-        }, {
-            joy_right: 50,
-            joy_left: 1,
-            joy_center: 25,
-            zoom: 75
-        }, {
-            joy_right: 50,
-            joy_left: 1,
-            joy_center: 25,
-            zoom: 10
-        }],
-        Display: [{}, {}, {}, {}]
+        Display: [{}]
     };
 
     angular.module('AcaEngine')
