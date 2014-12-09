01cc084385762026f492bad380756674c0fa6aa8
Stephen von Takach <steve@cotag.me>
2014-12-09 22:55:12 +1100

    add usyd branding

--- app/components/settings.js
+++ app/components/settings.js
@@ -69,10 +69,10 @@
                     this.$_self[parts.module][parts.index - 1].mute = true;
                 }
             },
-            "name": "Demo Room",
+            "name": "Carslaw Pod 158",
             "help_msg": "For help please call <strong>0408419954</strong>",
             "state": "shutdown",
-            "tab": "Camera",
+            "tab": "PC",
             "apps": ['explorer', 'firefox', 'vlc', 'sankore'],
             "channels": [
                 "7 Digital",
@@ -92,29 +92,14 @@
             ],
             "inputs": [
                 "PC",
-                "Laptop",
-                "Camera",
-                "VC"
+                "Laptop"
             ],
             "PC": [
-                "g1_pc1",
-                "g1_pc2",
-                "g2_pc1",
-                "g2_pc2"
+                "g1_pc1"
             ],
             "Laptop": [
                 "laptop_g1",
                 "laptop_g2"
-            ],
-            "Camera": [
-                "cam_r_g1",
-                "cam_f_g1",
-                "cam_r_g2",
-                "cam_f_g2"
-            ],
-            "VC": [
-                "vc1",
-                "vc2"
             ],
             "sources": {
                 "g1_pc1": {
@@ -123,79 +108,17 @@
                     "source": "hdmi",
                     "type": "residentpc"
                 },
-                "g1_pc2": {
-                    "title": "1G1 PC-2",
-                    "input": 15,
-                    "source": "hdmi",
-                    "type": "residentpc"
-                },
-                "g2_pc1": {
-                    "title": "1G2 PC-1",
-                    "input": 1,
-                    "source": "hdmi",
-                    "type": "residentpc"
-                },
-                "g2_pc2": {
-                    "title": "1G2 PC-2",
-                    "input": 5,
-                    "source": "hdmi",
-                    "type": "residentpc"
-                },
                 "laptop_g1": {
-                    "title": "1G1 Laptop",
+                    "title": "Laptop HDMI",
                     "input": 11,
                     "source": "hdmi",
                     "type": "aux_hdmi"
                 },
                 "laptop_g2": {
-                    "title": "1G2 Laptop",
+                    "title": "Laptop VGA",
                     "input": 2,
                     "source": "hdmi",
                     "type": "aux_hdmi"
-                },
-                "cam_r_g1": {
-                    "title": "1G1 Rear",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 1,
-                    "input": 12,
-                    "source": "hdmi"
-                },
-                "cam_f_g1": {
-                    "title": "1G1 Front",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 2,
-                    "input": 13,
-                    "source": "hdmi"
-                },
-                "cam_r_g2": {
-                    "title": "1G2 Rear",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 3,
-                    "input": 10,
-                    "source": "hdmi"
-                },
-                "cam_f_g2": {
-                    "title": "1G2 Front",
-                    "type": "vc-camera",
-                    "mod": "Camera",
-                    "index": 4,
-                    "input": 9,
-                    "source": "hdmi"
-                },
-                "vc1": {
-                    "title": "1G1 VC",
-                    "type": "vc-active",
-                    "input": 6,
-                    "content": 8
-                },
-                "vc2": {
-                    "title": "1G2 VC",
-                    "type": "vc-active",
-                    "input": 3,
-                    "content": 6
                 }
             },
             "outputs": {
@@ -212,98 +135,8 @@
                     "mixer_id": 105,
                     "type": "projector",
                     "pri": 1,
-                    "title": "G1 Front"
-                },
-                "Display_2": {
-                    "screen": {
-                        "module": "Screen_2",
-                        "index": 1
-                    },
-                    "output": [
-                        4,
-                        24
-                    ],
-                    "audio_out": 22,
-                    "mixer_id": 32,
-                    "type": "projector",
-                    "pri": 2,
-                    "title": "G1 Rear"
-                },
-                "VidConf_1": {
-                    "output": [
-                        7,
-                        9
-                    ],
-                    "no_audio": true,
-                    "no_mod": true,
-                    "type": "conference",
-                    "pri": 3,
-                    "title": "G1 VC"
-                },
-                "Display_3": {
-                    "screen": {
-                        "module": "Screen_3",
-                        "index": 1
-                    },
-                    "output": [
-                        1,
-                        21
-                    ],
-                    "audio_out": 23,
-                    "mixer_id": 107,
-                    "type": "projector",
-                    "pri": 4,
-                    "title": "G2 Front",
-                    "remote": true
-                },
-                "Display_4": {
-                    "screen": {
-                        "module": "Screen_4",
-                        "index": 1
-                    },
-                    "output": [
-                        2,
-                        22
-                    ],
-                    "audio_out": 24,
-                    "mixer_id": 106,
-                    "type": "projector",
-                    "pri": 5,
-                    "title": "G2 Rear",
-                    "remote": true
-                },
-                "VidConf_2": {
-                    "output": [
-                        5
-                    ],
-                    "no_audio": true,
-                    "no_mod": true,
-                    "type": "conference",
-                    "pri": 6,
-                    "title": "G2 VC",
-                    "remote": true
+                    "title": "Pod Display"
                 }
-            },
-            "light_level": "Off",
-            "lights": {
-                "levels": [
-                    {
-                        "name": "Full",
-                        "trigger": 3
-                    },
-                    {
-                        "name": "Presentation",
-                        "trigger": 2
-                    },
-                    {
-                        "name": "Off",
-                        "trigger": 1
-                    }
-                ],
-                "default": 3,
-                "shutdown": 0,
-                "present": 2,
-                "group": 12
             },
             "vol_max": 3,
             "vol_min": -50,
@@ -314,7 +147,7 @@
         Mixer: [{
             fader_106: 1,
             fader_107: 2,
-            fader_105: 3,
+            fader_105: -25,
             fader_32: -30,
             fader_107_mute: true, 
             $fader: function (fader, volume) {
@@ -324,43 +157,8 @@
                 this['fader_' + fader + '_mute'] = mute;
             } 
         }],
-        Switcher: [{}],
-        Lights: [{}],
-        Computer: [{}, {}, {}, {}],
-        Camera: [{
-            joy_right: 0x99,
-            joy_left: 0x1,
-            joy_center: 0x50,
-            zoom: 25
-        }, {
-            joy_right: 0x99,
-            joy_left: 0x1,
-            joy_center: 0x50,
-            zoom: 1
-        }, {
-            joy_right: 0x99,
-            joy_left: 0x1,
-            joy_center: 0x50,
-            zoom: 75
-        }, {
-            joy_right: 0x99,
-            joy_left: 0x1,
-            joy_center: 0x50,
-            zoom: 10
-        }],
+        Computer: [{}],
         Display: [{
-            $mute: function (mute) {
-                this.mute = mute;
-            }
-        }, {
-            $mute: function (mute) {
-                this.mute = mute;
-            }
-        }, {
-            $mute: function (mute) {
-                this.mute = mute;
-            }
-        }, {
             $mute: function (mute) {
                 this.mute = mute;
             }
