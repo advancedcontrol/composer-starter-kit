c391ec6f81b8ae6be5998dda16c1ec41a3da3248
Will Cannings <me@willcannings.com>
2015-02-12 01:52:09 +0700

    Sample impromptu content

--- app/components/meeting/meeting-ctrl.js
+++ app/components/meeting/meeting-ctrl.js
@@ -104,6 +104,104 @@
         ]
     };
 
+    var IMPROMPTU = {
+        name: 'Impromptu Meeting',
+        playlists: [
+            {
+                id: 1,
+                index: 1,
+                name: 'USB - Audio Content',
+                audio: true,
+                video: false,
+                duration: 300,
+                auto_next: false,
+                entries: [
+                    {
+                        id: 1,
+                        name: 'Track 1',
+                        duration: 100,
+                        transition: 2,
+                        index: 1,
+                        media_type: 2
+                    },
+                    {
+                        id: 2,
+                        name: 'Track 2',
+                        duration: 50,
+                        transition: 2,
+                        index: 2,
+                        media_type: 2
+                    },
+                    {
+                        id: 3,
+                        name: 'Track 3',
+                        duration: 150,
+                        transition: 2,
+                        index: 3,
+                        media_type: 2
+                    }
+                ]
+            },
+            {
+                id: 2,
+                index: 2,
+                name: 'USB - Image Content',
+                audio: false,
+                video: true,
+                duration: 75,
+                auto_next: false,
+                repeat: true,
+                entries: [
+                    {
+                        id: 4,
+                        name: 'Photo 1',
+                        duration: 15,
+                        transition: 2,
+                        index: 1,
+                        media_type: 1,
+                        default_poster_url: '/branding/images/poster1.jpg'
+                    },
+                    {
+                        id: 5,
+                        name: 'Photo 2',
+                        duration: 15,
+                        transition: 2,
+                        index: 2,
+                        media_type: 1,
+                        default_poster_url: '/branding/images/poster2.jpg'
+                    },
+                    {
+                        id: 6,
+                        name: 'Photo 3',
+                        duration: 15,
+                        transition: 2,
+                        index: 3,
+                        media_type: 1,
+                        default_poster_url: '/branding/images/poster3.jpg'
+                    },
+                    {
+                        id: 7,
+                        name: 'Photo 4',
+                        duration: 15,
+                        transition: 2,
+                        index: 4,
+                        media_type: 1,
+                        default_poster_url: '/branding/images/poster4.jpg'
+                    },
+                    {
+                        id: 8,
+                        name: 'Photo 5',
+                        duration: 15,
+                        transition: 2,
+                        index: 5,
+                        media_type: 1,
+                        default_poster_url: '/branding/images/poster5.jpg'
+                    }
+                ]
+            }
+        ]
+    };
+
     angular.module('AcaEngine')
     
         .controller('MeetingCtrl', [
@@ -111,9 +209,13 @@
             '$scope',
 
         function ($rootScope, $scope) {
-            $scope.meeting = MEETING;
             $scope.selected = null;
             $scope.playing = null;
+
+            if ($rootScope.meeting == 'impromptu')
+                $scope.meeting = IMPROMPTU;
+            else
+                $scope.meeting = MEETING;
 
             $scope.back = function() {
                 $rootScope.meeting = null;
