e85ee39573a5807245d161f78f2292b96f53e28d
Will Cannings <me@willcannings.com>
2015-03-16 14:11:20 +1100

    Page numbers, limit number of stream images to 9 to work around chrome bug

--- app/components/main-ctrl.js
+++ app/components/main-ctrl.js
@@ -1,21 +1,34 @@
 (function (angular) {
     'use strict';
+    var PER_PAGE = 9;
 
     angular.module('AcaEngine')
-
         .controller('MainCtrl', [
             '$scope',
 
             function ($scope) {
-                var cam = 1;
-
                 // The list of systems (listed in settings)
                 $scope.systems = window.systemsList;
 
-                $scope.nextCam = function () {
-                    cam = cam + 1;
-                    return cam;
-                };
+                // only PER_PAGE systems are shown at once to work around:
+                // https://code.google.com/p/chromium/issues/detail?id=362004
+                $scope.currentSystems = [];
+
+                // ng-repeat only works over collections
+                $scope.pageNums = [];
+                var numPages = Math.ceil($scope.systems.length / PER_PAGE);
+                for (var i = 1; i <= numPages; i++)
+                    $scope.pageNums.push(i);
+
+                $scope.gotoPage = function(pageNum) {
+                    if ($scope.page == pageNum)
+                        return;
+                    $scope.page = pageNum;
+                    var start = (pageNum - 1) * PER_PAGE;
+                    $scope.currentSystems = $scope.systems.slice(start, start + PER_PAGE);
+                }
+
+                $scope.gotoPage(1);
             }
         ]);
 
