b79f20a3114c142d50f0a5105d3f8e7554d5e4e4
Will Cannings <me@willcannings.com>
2015-04-27 14:43:21 +1000

    Use default_settings.js

--- app/components/settings.js
+++ app/components/settings.js
@@ -46,45 +46,6 @@
     };
 
     angular.module('AcaEngine')
-    
-        // The authentication service doesn't have to be hosted on the same domain
-        // as the control service - hence the complexity of this configuration
-        .config([
-            '$composerProvider',
-            '$locationProvider',
-
-        function(comms) {
-            // Point these variables to your ACA Engine instance
-            // to start interacting with it using ACA Composer
-            comms.port  = 3000;
-            comms.host  = 'localhost';
-            comms.tls   = false;
-
-            // This outputs debugging information to console useful
-            // if you want to see the communications occurring
-            // between the interface and ACA Engine.
-            //
-            // Should be commented out for production
-            comms.debug = true;
-
-            // If you would like to use Authentication then you
-            // must point this configuration to your compatible oauth server
-            //
-            // It's a lazy authentication process so only if a request
-            // is made to a protected endpoint or a 401 is received will
-            // the auth process activate.
-            comms.useService({
-                id: 'AcaEngine',
-                scope: 'public',
-                oauth_server: 'http://localhost:9000/auth/oauth/authorize',
-                oauth_tokens: 'http://localhost:9000/auth/token',
-                redirect_uri: 'http://localhost:9000/oauth-resp.html',
-                client_id: 'df46d04043f6fe1d9949d9effba43b25b664064addfe4670aae8a24fe3f3f570',
-                api_endpoint: '/api',
-                proactive: true
-            });
-        }])
-
         .run([
             '$window',
             '$location',
