d2b819e1254990dea383306725a21b90ae9bfedd
Will Cannings <me@willcannings.com>
2015-05-29 02:00:41 +1000

    Remove unneeded settings code

--- app/components/settings.js
+++ app/components/settings.js
@@ -8,6 +8,8 @@
     window.systemData['sys-B0'] = {
         System: [{
             "title": "Studio Projector",
+            "tours_pin": "1234",
+            "tech_pin": "1978",
 
             // power on/off
             "state": "shutdown",
@@ -51,49 +53,6 @@
     };
 
     angular.module('AcaEngine')
-    
-        // The authentication service doesn't have to be hosted on the same domain
-        // as the control service - hence the complexity of this configuration
-        .config([
-            '$composerProvider',
-            '$locationProvider',
-
-        function(comms) {
-            // Point these variables to your ACA Engine instance
-            // to start interacting with it using ACA Composer
-            //comms.port  = 80;
-            //comms.host  = '10.71.1.248';
-            comms.tls   = false;
-
-            // This outputs debugging information to console useful
-            // if you want to see the communications occurring
-            // between the interface and ACA Engine.
-            //
-            // Should be commented out for production
-            comms.debug = true;
-
-            // If you would like to use Authentication then you
-            // must point this configuration to your compatible oauth server
-            //
-            // It's a lazy authentication process so only if a request
-            // is made to a protected endpoint or a 401 is received will
-            // the auth process activate.
-            comms.useService({
-                id: 'AcaEngine',
-                scope: 'public',
-                oauth_server: 'http://sohiptva.soh.com/auth/oauth/authorize',
-                oauth_tokens: 'http://sohiptva.soh.com/auth/token',
-                redirect_uri: 'http://sohiptva.soh.com/oauth-resp.html',
-                client_id: 'df46d04043f6fe1d9949d9effba43b25b664064addfe4670aae8a24fe3f3f570',
-                login_redirect: function () {
-                    var url = encodeURIComponent(document.location.href);
-                    return '/auth/login?continue=' + url + '&provider=soh';
-                },
-                api_endpoint: '/api',
-                proactive: true
-            });
-        }])
-
         .run([
             '$window',
             '$location',
