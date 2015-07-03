1d588b03b89bba5bdeb5c5095f06cc799b0bd27e
Stephen von Takach <steve@cotag.me>
2015-07-03 14:07:17 +1000

    add bootstrap for simplified setup

---
+++ app/bootstrap/setup-ctrl.js
@@ -0,0 +1,49 @@
+(function (window, angular) {
+    'use strict';
+
+
+    var bootstrapKey = 'aca.meeting.bootstrap',
+        redirectTo = function (system_id) {
+            var newUrl,
+                index;
+
+            newUrl = window.location.href;
+            index = newUrl.lastIndexOf('/') + 1;
+            newUrl = newUrl.substr(0, index) + '#/?trust=true&ctrl=' + system_id;
+
+            // Perform the redirect
+            window.location.href = newUrl;
+        };
+
+
+    angular.module('AcaBootstrap', [
+        'ngGesture'  // Gesture events
+    ])
+
+
+    .run(['$rootScope', function ($rootScope) {
+        var system_id = localStorage.getItem(bootstrapKey);
+
+        if (system_id) {
+            redirectTo(system_id);
+        } else {
+            $rootScope.loaded = true;
+        }
+    }])
+
+
+    .controller('SetupCtrl', [
+        '$scope',
+    function ($scope) {
+
+        $scope.configure = function (system_id) {
+            // Ensure we are saving a valid value
+            if (system_id && system_id.length > 5) {
+                localStorage.setItem(bootstrapKey, system_id);
+                redirectTo(system_id);
+            }
+        };
+
+    }]);
+
+}(this, this.angular));
