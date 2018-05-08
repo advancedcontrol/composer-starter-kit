8e24deec4bd725d9217538a3d23bfba3f84a901c
Stephen von Takach <steve@cotag.me>
2018-05-09 07:39:54 +1000

    improve dial pad

--- app/components/main/Phone/phone-ctrl.js
+++ app/components/main/Phone/phone-ctrl.js
@@ -23,22 +23,26 @@
             });
 
             $scope.addNum = function (val) {
+                if (phone.number && phone.number.length)
+                        phone.number += val;
+                    else
+                        phone.number = '' + val;
+
                 if (phone.ringing || phone.incall) {
                     if (sendSpace) {
                         val = val + ' ';
                     }
                     $scope.coModuleInstance.$exec('set_string', $scope.phone_settings.dtmf, val);
                     sendSpace = !sendSpace;
-                } else {
-                    if (phone.number && phone.number.length)
-                        phone.number += val;
-                    else
-                        phone.number = '' + val;
                 }
             };
 
             $scope.backspace = function () {
                 phone.number = phone.number.substring(0, phone.number.length - 1);
+            };
+
+            $scope.clear = function () {
+                phone.number = '';
             };
         }]);
 
